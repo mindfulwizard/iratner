f0841258fa19d7881b2622f3af760b79269d2b1e
Isaac Ratner <isaacratner@gmail.com>
2015-11-19 13:30:30 -0500

    deadend

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
-var app = angular.module('iratner', ['ngRoute']);
+var app = angular.module('iratner', ['ngRoute','ngMockE2E']);
+
+app.run(function($httpBackend) {
+    $httpBackend.whenGET('/home.html').passThrough();
+    $httpBackend.whenGET('/articles.html').passThrough();
+    $httpBackend.whenGET('/projects.html').passThrough();
+    $httpBackend.whenGET('/about.html').passThrough();
+    $httpBackend.whenGET('pages/home.html').passThrough();
+    $httpBackend.whenGET('pages/about.html').passThrough();
+    $httpBackend.whenGET('pages/articles.html').passThrough();
+    $httpBackend.whenGET('pages/projects.html').passThrough();
+});
 
 app.config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
+
 	$routeProvider
         .when('/', {
             templateUrl: 'pages/home.html',
             controller: 'MainCtrl'
         })
-        .when('/about', {
+        .when('/index.html', {
+            templateUrl: 'pages/home.html',
+            controller: 'MainCtrl'
+        })
+        .when('/about.html', {
             templateUrl: 'pages/about.html'
         })
-        .when('/articles', {
+        .when('/articles.html', {
             templateUrl: 'pages/articles.html'
         })
-        .when('/projects', {
+        .when('/projects.html', {
             templateUrl: 'pages/projects.html'
         });
-    });
+});
 
-app.controller('MainCtrl', function ($scope) {
-
+app.controller('MainCtrl', function ($scope, $window) {
+    this.goToTemplate = function(template) {
+        $window.location.href = '/pages/' + template + '.html';
+    };
     $scope.tab = 1;
 
     $scope.setTab = function(newTab){
